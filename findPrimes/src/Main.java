f11eef0292e83e5be87e9d4853ddf6b60ef03bb9
Gustavo da Silva Oliveira <silva.gustavo98@gmail.com>
2025-08-19 15:29:51 -0300

    Find Primes finalizado

--- findPrimes/src/Main.java
+++ findPrimes/src/Main.java
@@ -1,18 +1,43 @@
 import IsPrime.PrimeValidator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> primesFound = new ArrayList<>(Arrays.asList(2));
+
+        // Bloco que lê entrada do usuário
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o número de primos: ");
         int sizeArray = scanner.nextInt();
-        System.out.println(sizeArray);
+
+        //Trecho de contagem tempo
+        long inicio = System.currentTimeMillis();
+
+        ArrayList<Integer> primesFound = new ArrayList<>(List.of(2));
+
+        if (sizeArray == 1) {
+            System.out.println(primesFound);
+            long fim = System.currentTimeMillis();
+            long tempoDecorrido = fim - inicio;
+            System.out.println("Tempo decorrido:" + tempoDecorrido + "ms");
+            return;
+        }
+
         PrimeValidator primeValidator = new PrimeValidator();
-        boolean b = primeValidator.validatePrime(2,primesFound);
-        System.out.println(b);
+        int i = 3;
+        do {
+            boolean isPrime = primeValidator.validatePrime(i,primesFound);
+            if (isPrime) {
+                primesFound.add(i);
+            }
+            i++;
+        }
+        while (primesFound.size() < sizeArray);
+
+        long fim = System.currentTimeMillis();
+        long tempoDecorrido = fim - inicio;
+        System.out.println("Tempo decorrido:" + tempoDecorrido + "s");
     }
 }
